114ec835640c76fb6ecba8a64828dafb1eeb2026
gourav sasson <sasson.gourav93@gmail.com>
2025-01-19 14:20:29 +0530

    change

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,21 +1,21 @@
-import logo from "../../assets/Logo.png"
+import logo from "../../assets/footerlogo.png"
  function Footer() {
   return (
     
 
-<footer className="bg-[#f9cf27] dark:bg-gray-900">
+<footer className="bg-[#424241] dark:bg-gray-900">
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href={logo} className="flex items-center">
-                  <img src={logo} className=" w-[148px] me-3" alt="FlowBite Logo" />
+                  <img src={logo} className=" w-[120px] me-3" alt="FlowBite Logo" />
                   
               </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                  <ul className="text-[#141618] dark:text-gray-400 font-medium">
+                  <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Resources</h2>
+                  <ul className="text-white dark:text-gray-400 font-medium">
                       <li className="mb-4">
                           <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
                       </li>
@@ -25,8 +25,8 @@ import logo from "../../assets/Logo.png"
                   </ul>
               </div>
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                  <ul className="text-[#141618] dark:text-gray-400 font-medium">
+                  <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Follow us</h2>
+                  <ul className="text-white dark:text-gray-400 font-medium">
                       <li className="mb-4">
                           <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
                       </li>
@@ -36,8 +36,8 @@ import logo from "../../assets/Logo.png"
                   </ul>
               </div>
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                  <ul className="text-[#141618] dark:text-gray-400 font-medium">
+                  <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Legal</h2>
+                  <ul className="text-white dark:text-gray-400 font-medium">
                       <li className="mb-4">
                           <a href="#" className="hover:underline">Privacy Policy</a>
                       </li>
--- src/component/Ourservices/Service.tsx
+++ src/component/Ourservices/Service.tsx
@@ -1,29 +1,33 @@
 const data =[
 
     {
-        title: "IMAGERY",
-        details:"Engaging images for marketing and competitions, from large urban to architecture scale. Our CGIs are recognized by their level of quality with a competitive turnaround using the latest smart parametric and procedural modelling software."
+        title: "Exterior CGI",
+        details:"Stunning, lifelike renders that showcase the elegance and structure of buildings."
     },
     {
-        title: "ANIMATION",
-        details:"Tell a story with an animation - created from scratch using the virtual environments, sound design, visual FX and cutting-edge motion graphics."
+        title: "Interior CGI",
+        details:"Detailed visuals that capture the mood, texture, and essence of interior spaces."
     },
     {
-        title: "VIRTUAL REALITY",
-        details:"Realtime-rendered environments for urban designers, architects or developers. Our development team are constantly working on solutions that offer an immersive environment for design development and high end marketing."
+        title: "3D Video Animations",
+        details:"Dynamic walkthroughs that provide a cinematic experience of your project."
+    },
+    {
+        title: "360° Virtual Tours",
+        details:"Interactive experiences that give your clients a complete perspective."
     }
 ]
 
 function Service() {
   return (
     <>
-    <div className="mx-[345px] mt-[136px]">
-    <div className=" p-10 pb-0 text-3xl text-white">Our <strong>Service</strong></div>
-    <div className=" p-10 w-full h-full flex justify-between">
+    <div className="mx-[200px] mt-[136px]">
+    <div className=" p-10 pb-0 text-3xl text-white">What We Do</div>
+    <div className=" p-10 w-full h-full flex justify-between gap-10">
      
             {data.map((item, index) => (
                 <div key={index} className="mb-10 basis-[30%]">
-                    <h3 className=" text-sm font-medium text-[#f9cf27] mb-4">{item.title}</h3>
+                    <h3 className=" text-sm font-bold text-[#424241] mb-4">{item.title}</h3>
                     <p className=" text-white">{item.details}</p>
                 </div>
             ))}
